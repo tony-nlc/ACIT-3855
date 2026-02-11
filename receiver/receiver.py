4c13aa686111f852d2c5ddae9e0f169bd4491846
Tony <ngai.lam.chou@gmail.com>
2026-02-11 09:07:33 -0800

    Sync all files

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -61,7 +61,7 @@ def process_exercise_batch(body):
     return NoContent, 201
 
 app = connexion.FlaskApp(__name__, specification_dir="")
-app.add_api("openapi_copy.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     app.run(port=8080)
